da9550cade0a4443028f8a1785657c2e9546da7b
fengxiaomao <fxm@autoforce.net>
2018-10-13 10:26:56 +0800

    vinyl-fs switch

--- bin/gulp.js
+++ bin/gulp.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+/*jshint node:true */
+
+'use strict';
+
 var path = require('path');
 
 var argv = require('optimist').argv;
@@ -133,7 +137,7 @@ function loadGulpFile(localGulp, gulpFile, tasks) {
   return theGulpfile;
 }
 
-function getGulpFile(baseDir) {
+function getGulpFile() {
   var extensions;
   if (require.extensions) {
     extensions = Object.keys(require.extensions).join(',');
